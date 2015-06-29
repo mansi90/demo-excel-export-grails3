b37da623705d89dcf447b9273341f9751e3dc3b3
mansiarora <mansi@intelligrape.com>
2015-06-29 17:41:53 +0530

    Created Product domain; Added bootstrap data;

---
+++ grails-app/domain/sample/excel/export/Product.groovy
@@ -0,0 +1,11 @@
+package sample.excel.export
+
+class Product {
+
+    String name
+    String description
+    Float price = 0
+
+    static constraints = {
+    }
+}
--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -1,6 +1,12 @@
+import sample.excel.export.Product
+
 class BootStrap {
 
     def init = { servletContext ->
+
+        (1..10).each {
+            new Product(name: "Product ${it}", description: "Some description text -  ${it}", price: it * 100).save(flush: true)
+        }
     }
     def destroy = {
     }
